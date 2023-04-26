55af8bf3df4a4dc53e89499abb7f65fd8a838139
StayDare <staydare0914@gmail.com>
2023-04-27 02:06:49 +0800

    frame2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,30 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'restaurant',
-    loadChildren: () => import('./restaurant/restaurant.module').then( m => m.RestaurantPageModule)
-  },
-  {
-    path: 'fastfood',
-    loadChildren: () => import('./fastfood/fastfood.module').then( m => m.FastfoodPageModule)
-  },
-  {
-    path: 'milkytea',
-    loadChildren: () => import('./milkytea/milkytea.module').then( m => m.MilkyteaPageModule)
-  },
-  {
-    path: 'tab',
-    loadChildren: () => import('./tab/tab.module').then( m => m.TabPageModule)
-  },
+    loadChildren:() => import('./tab/tab.module').then(m => m.TabPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/tab/tab-routing.module.ts
+++ src/app/tab/tab-routing.module.ts
@@ -4,8 +4,33 @@ import { TabPage } from './tab.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tab',
     component: TabPage,
+    children:[
+      //home
+      {
+        path: 'milkytea',
+        loadChildren:() => import('../milkytea/milkytea.module').then(m => m.MilkyteaPageModule)
+      },
+      {
+        path: 'restaurant',
+        loadChildren:() => import('../restaurant/restaurant.module').then(m => m.RestaurantPageModule)
+      },
+      {
+        path: 'fastfood',
+        loadChildren:() => import('../fastfood/fastfood.module').then(m => m.FastfoodPageModule)
+      },
+      {
+        path: 'home',
+        loadChildren:() => import('../home/home.module').then(m => m.HomePageModule)
+      }
+    ]
+  },
+
+  {
+    path:'',
+    redirectTo:'/tab/home',
+    pathMatch:'full'
   }
 ];
 
