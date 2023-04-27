2f7957da4c3ccf7ffbfd1f1d746eb50d9e9e2401
StayDare <staydare0914@gmail.com>
2023-04-27 23:04:12 +0800

    2.3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { ProductService, product} from '../product.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+items:product[]
+  constructor(private productService:ProductService) {
+    this.items = this.productService.getProducts()
+  }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,12 +19,12 @@ export class ProductService {
 //构造函数，一个类只能有一个
  constructor() { 
   this.products = [
-    {id:0,name:"炒米粉",image:"1",price:12,type:"restaurant"},
-    {id:1,name:"披萨",image:"2",price:12,type:"fastfood"},
-    {id:2,name:"烤鸭",image:"3",price:12,type:"restaurant"},
-    {id:3,name:"皮蛋粥",image:"4",price:12,type:"restaurant"},
-    {id:4,name:"炸鸡",image:"5",price:12,type:"fastfood"},
-    {id:5,name:"蟹堡",image:"6",price:12,type:"restaurant"},
+    {id:0,name:"炒米粉",image:"./assets/img/chaomifen.jpg",price:12,type:"restaurant"},
+    {id:1,name:"披萨",image:"./assets/img/pisa.jpg",price:12,type:"fastfood"},
+    {id:2,name:"烤鸭",image:"./assets/img/kaoya.jpg",price:12,type:"restaurant"},
+    {id:3,name:"皮蛋粥",image:"./assets/img/pidanzhou.jpg",price:12,type:"restaurant"},
+    {id:4,name:"炸鸡",image:"./assets/img/zhaji.jpg",price:12,type:"fastfood"},
+    {id:5,name:"蟹堡",image:"./assets/img/xiebao.jpg",price:12,type:"restaurant"},
    
   ] 
 }
