ba47979887fcfedee61bb9064ab422e6c6371f48
Murasame <me@murasame.moe>
2023-05-08 00:57:31 +0800

    Add back to top

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductService, product} from '../product.service';
+import { IonContent, Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-items:product[]
+  @ViewChild(IonContent) content!: IonContent;
+  items:product[]
   constructor(private productService:ProductService) {
     this.items = this.productService.getProducts()
+  }
+
+  scrollToTop(): void {
+    this.content?.scrollToTop(0);
   }
 
 }
